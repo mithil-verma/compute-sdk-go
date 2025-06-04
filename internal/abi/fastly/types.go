0a6b2abc15fb9e05741b0827babd5f88408c7018
Mithil Verma <mithilverma@A16095.local>
2025-06-04 18:34:44 -0400

    add change

--- internal/abi/fastly/types.go
+++ internal/abi/fastly/types.go
@@ -1537,10 +1537,10 @@ type httpCacheHitCount prim.U64
 
 const (
 	// Insert the response into cache (`transaction_insert*`).
-	HTTPCacheStorageActionInsert HTTPCacheStorageAction = 0
+	HTTPCacheStorageActionInsert HTTPCacheStorageAction = 1
 
 	// Update the stale response in cache (`transaction_update*`).
-	HTTPCacheStorageActionUpdate HTTPCacheStorageAction = 1
+	HTTPCacheStorageActionUpdate HTTPCacheStorageAction = 0
 
 	// Do not store this response.
 	HTTPCacheStorageActionDoNotStore HTTPCacheStorageAction = 2
